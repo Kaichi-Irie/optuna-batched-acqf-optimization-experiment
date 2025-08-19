2057df911e12b78962e6f5ff8b942a92aa632069
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-08-19 13:40:42 +0900

    Refactor BatchedSampler to include mode parameter for improved multiprocessing support

--- batched_sampler.py
+++ batched_sampler.py
@@ -1,25 +1,32 @@
 import multiprocessing
+from typing import Literal
 
 import numpy as np
 import optuna._gp.acqf as acqf_module
 from optuna.samplers import GPSampler
 
-import batched_optim_mixed
+import multiprocessing_optim_mixed
+import stacking_optim_mixed
+
+SAMPLERMODE = Literal["stacking", "batched_acqf_eval", "multiprocessing"]
 
 
 class BatchedSampler(GPSampler):
-    def __init__(self, batch_size: int, *args, **kwargs):
+    def __init__(self, batch_size: int, mode: SAMPLERMODE, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.batch_size: int = batch_size
         self.worker_pool = None
         self.processes: int | None = None
+        self.mode: SAMPLERMODE = mode
 
     def create_worker_pool(self, processes: int):
+        if self.mode != "multiprocessing":
+            raise ValueError(f"Invalid mode: {self.mode}.")
         self.processes = processes
         self.worker_pool = multiprocessing.Pool(processes=processes)
 
     def shutdown_worker_pool(self):
-        if self.worker_pool is not None:
+        if self.mode == "multiprocessing" and self.worker_pool is not None:
             self.worker_pool.close()
             self.worker_pool.join()
             self.worker_pool = None
@@ -29,13 +36,27 @@ class BatchedSampler(GPSampler):
         self, acqf: acqf_module.BaseAcquisitionFunc, best_params: np.ndarray | None
     ) -> np.ndarray:
         assert best_params is None or len(best_params.shape) == 2
-        normalized_params, _acqf_val = batched_optim_mixed.optimize_acqf_mixed(
-            acqf,
-            worker_pool=self.worker_pool,
-            warmstart_normalized_params_array=best_params,
-            n_preliminary_samples=self._n_preliminary_samples,
-            n_local_search=self._n_local_search,
-            tol=self._tol,
-            rng=self._rng.rng,
-        )
-        return normalized_params
+
+        if self.mode == "stacking":
+            raise ValueError("Stacking mode is not implemented.")
+        if self.mode == "batched_acqf_eval":
+            raise ValueError(
+                "Batched acquisition function evaluation is not implemented."
+            )
+        if self.mode == "multiprocessing":
+            if self.worker_pool is None:
+                raise ValueError("Worker pool must be created before optimization.")
+            normalized_params, _acqf_val = (
+                multiprocessing_optim_mixed.optimize_acqf_mixed(
+                    acqf,
+                    worker_pool=self.worker_pool,
+                    warmstart_normalized_params_array=best_params,
+                    n_preliminary_samples=self._n_preliminary_samples,
+                    n_local_search=self._n_local_search,
+                    tol=self._tol,
+                    rng=self._rng.rng,
+                )
+            )
+            return normalized_params
+        else:
+            raise ValueError(f"Invalid mode: {self.mode}.")
