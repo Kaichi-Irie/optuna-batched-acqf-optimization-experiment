15f336cbfe24d393d41566c057837615f77c45b6
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-08-29 11:01:04 +0900

    Enhance visualization of speedup comparison by adding bar plot and formatting adjustments

--- pandasvisualize.py
+++ pandasvisualize.py
@@ -44,10 +44,90 @@ summary_with_ratio["speedup"] = (
     summary_with_ratio["original_elapsed"] / summary_with_ratio["elapsed_mean"]
 ) * 100
 
-# 表示用にフォーマットを整える
-summary_with_ratio["speedup"] = summary_with_ratio["speedup"].map("{:.2f}%".format)
-
+# 整数に丸める
+summary_with_ratio["speedup"] = summary_with_ratio["speedup"].map(round)
 
 summary_with_ratio
+
+# %%
+# dimension毎にspeedupの平均を計算
+mean_speedup = (
+    summary_with_ratio.groupby(["dimension", "mode"])["speedup"].mean().reset_index()
+)
+mean_speedup
+
+
+# %%
+# 棒グラフにプロット
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.barplot(data=mean_speedup, x="dimension", y="speedup", hue="mode")
+# set y-axis title
+plt.xlabel("Dimension of the Problem")
+plt.ylabel("Mean Speedup (%)")
+plt.title("Mean Speedup Compared to Original by Dimension and Mode")
+# %%
+import matplotlib.pyplot as plt
+import pandas as pd  # サンプルデータ作成のため
+import seaborn as sns
+
+# ----------------------------------------------------
+
+# グラフのスタイルとフォントサイズを設定
+sns.set_theme(style="whitegrid")
+sns.set_context("talk", font_scale=1.7)
+
+# グラフのサイズを指定
+plt.figure(figsize=(12, 9))
+# plt.figure(figsize=(7.5, 10))
+# fig, ax = plt.subplots(figsize=(7, 12))
+
+hue_order = [
+    # "original",
+    "multiprocessing",
+    "stacking",
+    "batched_acqf_eval",
+]
+
+palette = {
+    # "original": "gray",
+    "multiprocessing": "darkgray",  # 灰色
+    "stacking": "#0A00C9",
+    "batched_acqf_eval": "#9E0404",  # 赤
+}
+
+# グラフの描画
+mean_speedup_5D = mean_speedup[mean_speedup["dimension"] == 5]
+mean_speedup_wo_original = mean_speedup[mean_speedup["mode"] != "original"]
+ax = sns.barplot(
+    data=mean_speedup_wo_original,
+    x="dimension",
+    y="speedup",
+    hue="mode",
+    hue_order=hue_order,
+    palette=palette,
+    legend=False,
+)
+
+
+ax.axhline(
+    100, color="black", linestyle="--", linewidth=1.5, label="Baseline (Original)"
+)
+
+# ラベルとタイトルの設定
+plt.xlabel("Dimension")
+plt.ylabel("Mean Speedup (%)")
+plt.title("Speedup Comparison")
+
+# 凡例の調整
+# plt.legend(title="Method", bbox_to_anchor=(1.02, 1), loc="lower center", ncols=2)
+# ax.legend()
+
+# レイアウトを自動調整
+plt.tight_layout()
+
+# グラフを表示
+plt.savefig("speedup_comparison.png", dpi=900, bbox_inches="tight")
 
 # %%
